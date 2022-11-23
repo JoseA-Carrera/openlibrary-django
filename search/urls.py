87f4102ab22c96cc6035d7d40b439bde02b45dc9
Jose Alveiro <jose2252017@gmail.com>
2022-11-23 16:09:34 -0500

    agregando vista y template a authors

--- search/urls.py
+++ search/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.books, name='books'),
+    path('authors/', views.authors, name='authors')
 ]
--- search/views.py
+++ search/views.py
@@ -1,27 +1,82 @@
 from django.shortcuts import render
 import requests as rq
 
-def index(request):
+def books(request):
     try:
-        query_set = request.GET.get('title')
+        query_set = request.GET.get('name')
         url = 'http://openlibrary.org/search.json?title=' + query_set
         response =  rq.get(url)
         data = response.json()
         data_dict = dict(data)
-        titles = []
         docs = data_dict.get('docs') 
 
-        for title in docs:
-            t = title.get('title')
-            titles.append(t)
+        book = {} # 'book' almacenara de manera anidada todos los datos al template siendo las keys lo titulos
 
-        print(titles)
+        for key in docs: # 'key' iterara dentro de docs todas las keys que traiga la consulta requests
+            values = [] # 'values' sera una lista que almacenara la url de imagen y nombre de author
+
+
+            cover = key.get('cover_i')
+            title = key.get('title')
+            author =  key.get('author_name')
+
+            if cover == None: # al la key que no tenga url de imagen 'cover' traera de internet una img 404
+                cover = 'https://www.404.agency/static/images/404-share-image.png'
+            else:
+                cover = f'https://covers.openlibrary.org/b/id/{cover}-M.jpg'
+
+            values.append(cover)
+            values.append(author)
+
+            dic = { title: values }
+            book |= dic # 'book' concatenara a cada diccionario'dic' del momento
+
 
         if response.status_code == 200:
-            return render(request, 'index.html', {'xd': titles})
+            return render(request, 'index.html', {'books': book})
 
         else:
             return render(request, 'index.html')
 
     except:
         return render(request, 'index.html',)
+
+def authors(request):
+    try:
+        query_set = request.GET.get('name') 
+        url = 'https://openlibrary.org/search/authors.json?q=' + query_set
+        response =  rq.get(url)
+        data = response.json()
+        data_dict = dict(data)
+        docs = data_dict.get('docs') 
+        book = {}
+
+        for key in docs:
+            values = []
+
+            photo = key.get('key')
+            name = key.get('name')
+            top = key.get('top_work')
+            birth =  key.get('birth_date')
+            death = key.get('death_date')
+
+            photo = f'https://covers.openlibrary.org/a/olid/{photo}-M.jpg'
+            
+            if death == None:
+                death = 'now'
+
+            values.append(photo)
+            values.append(top)
+            values.append(birth)
+            values.append(death)
+
+            dic = { name: values }
+            book |= dic
+
+        if response.status_code == 200:
+            return render(request, 'author.html', {'authors': book})
+        else:
+            return render(request, 'author.html')
+
+    except:
+        return render(request, 'author.html',)
